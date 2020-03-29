94d9519c79ec1ad99a8bb27fa1c67912b569ea9f
hamstergeek <hamstergeek38@gmail.com>
2020-03-29 09:45:16 +0700

    membuat helpers dan provider

---
+++ src/Helpers/PersegiPanjang.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Hamstergeek\BangunRuang\Helpers;
+
+
+class PersegiPanjang
+{
+    public function luas($panjang, $lebar) {
+        return $panjang * $lebar;
+    }
+}
---
+++ src/Providers/BangunRuangServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Hamstergeek\BangunRuang\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class BangunRuangServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
